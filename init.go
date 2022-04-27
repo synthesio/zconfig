f2f740ed809cf2f8b8fc1a3fcea98be5b2ab3caf
David Bariod <dbariod@synthesio.com>
2022-04-27 16:14:47 +0200

    keep initializing struct without context

--- init.go
+++ init.go
@@ -10,19 +10,36 @@ type Initializable interface {
 	Init(context.Context) error
 }
 
+type initializableDeprecated interface {
+	Init() error
+}
+
 // Used for type comparison.
 var typeInitializable = reflect.TypeOf((*Initializable)(nil)).Elem()
+var typeInitializableDeprecated = reflect.TypeOf((*initializableDeprecated)(nil)).Elem()
 
 func Initialize(ctx context.Context, field *Field) error {
 	// Not initializable, nothing to do.
-	if !field.Value.Type().Implements(typeInitializable) {
+	if field.Value.Type().Implements(typeInitializable) {
+
+		// Initialize the element itself via the interface.
+		err := field.Value.Interface().(Initializable).Init(ctx)
+		if err != nil {
+			return fmt.Errorf("initializing field: %s", err)
+		}
+
 		return nil
 	}
 
-	// Initialize the element itself via the interface.
-	err := field.Value.Interface().(Initializable).Init(ctx)
-	if err != nil {
-		return fmt.Errorf("initializing field: %s", err)
+	if field.Value.Type().Implements(typeInitializableDeprecated) {
+
+		// Initialize the element itself via the interface.
+		err := field.Value.Interface().(initializableDeprecated).Init()
+		if err != nil {
+			return fmt.Errorf("initializing field: %s", err)
+		}
+
+		return nil
 	}
 
 	return nil
